d04682f3adecd2242236264b0310730587b6239f
G Chow <grendon.chow@gmail.com>
2022-02-22 20:20:52 +0800

    Added noted on other improved solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
 const removeFromArray = function(arr, ... removeArr) {
+    //JS has an includes() method that checks if array has value
+    //for loop can be replace using includes to push values into new array
+    // arr.forEach(item => {
+    //     if(!removeArr.includes(item))
+    //     {
+    //          newArray.push(item)
+    //     }
+    // })
+    // Another way is to combine filter where it filters and builds an array
+    // that does not imclude the values.
+    // return arr.filter(val => !removeArr.includes(val));
     for(let i = 0; i < removeArr.length; i++)
     {
         for(let j = 0; j < arr.length; j++)
