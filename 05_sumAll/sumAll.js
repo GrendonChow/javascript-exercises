53cc80317f76389af785e8c8d42efdc0369b0944
G Chow <grendon.chow@gmail.com>
2022-02-22 20:33:09 +0800

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    args.sort();
+    args[0];
+    args[1];
+    if(args[0] < 0|| args[1] < 0)
+    {
+        return 'ERROR';
+    }
+    if(typeof args[0] != 'number' || typeof args[1] != 'number')
+    {
+        return 'ERROR';
+    }
+    let result = 0;
+    for(let i = args[0]; i <= args[1]; i++)
+    {
+        result+= i;
+    }
+    return result;
 };
 
 // Do not edit below this line
