fe2491a088c7794aac400850b97a54f8acc75f9e
G Chow <grendon.chow@gmail.com>
2022-02-22 16:07:58 +0800

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str, count ) {
+    let result = '';
+    if(count < 0)
+    {
+        result = 'ERROR';
+    }
+    else
+    {
+      for(let i = 0; i < count; i++)
+      {   
+        result+= str;
+      }
+    }
+    return result;
 };
 
 // Do not edit below this line
