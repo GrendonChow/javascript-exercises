198f43c3f3c448f79789e495cd62d94224ecac07
G Chow <grendon.chow@gmail.com>
2022-02-22 20:11:50 +0800

    completed excersise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ... removeArr) {
+    for(let i = 0; i < removeArr.length; i++)
+    {
+        for(let j = 0; j < arr.length; j++)
+        {
+            if(arr[j] === removeArr[i])
+            {
+                arr[j] = null;
+            }
+        }
+    }
+    arr = arr.filter(x => x != null)
+    return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
