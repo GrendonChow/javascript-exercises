f74741e57fb56548238f7e2b3d439c8720e68632
G Chow <grendon.chow@gmail.com>
2022-02-22 16:23:26 +0800

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+const splitstr = str.split("");
+let result = '';
+//JS has a reverse() function, must split string to an array, reverse then join
+//back into whole string.
+//result = str.split("").reverse().join('');
+for(const c of splitstr)
+{
+    result = c + result;
+}
+return result;
 };
 
 // Do not edit below this line
