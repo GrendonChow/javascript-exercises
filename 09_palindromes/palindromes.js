2cbc06f917ab67448a41dc12ecbb903ef49df810
G Chow <grendon.chow@gmail.com>
2022-02-24 13:48:42 +0800

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    //regex is used to remove non alphanumeric chars
+    str = str.replace(/\W/g,'').toLowerCase();
+    //Converts the str to an char seperated array to use Array.reverse()
+    //then joins array back to a string.
+    reverseStr = str.split('').reverse().join('');
+    return reverseStr === str;
 };
 
 // Do not edit below this line
