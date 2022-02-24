d2e7e642c77045cfe7f77fc8c75b97c85b5dc37d
G Chow <grendon.chow@gmail.com>
2022-02-24 14:43:35 +0800

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    //reducer will iterate through objects and return value of anom function
+    const reducer = (accum, curr) => {
+        currAge = calcAge(curr.yearOfBirth, curr.yearOfDeath);
+        accumAge = calcAge(accum.yearOfBirth, accum.yearOfDeath);
+        if(currAge > accumAge)
+        {
+            accum = curr;
+        }
+        return accum;
+    }
+    result = people.reduce(reducer)
+    return result;
 };
+
+//refactored to use death year rather than person object to decouple.
+const calcAge = function(birth, death)
+{
+    if(!death)
+    {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
