a14635389fb911c7b3355a013d5343b36d4bdc7e
G Chow <grendon.chow@gmail.com>
2022-02-24 13:38:02 +0800

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length == 0)
+  {
+    return 0;
+  }
+  const reducer = (accum, curr) => accum + curr;
+  return arr.reduce(reducer);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const reducer = (accum, curr) => accum * curr;
+  return arr.reduce(reducer);
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+	return num ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	for(let i = num; i > 0; i--)
+  {
+      result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
