3d3a92454736211c6f8fb973dca7305477c1092c
G Chow <grendon.chow@gmail.com>
2022-02-24 14:04:19 +0800

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const arr = [ 0, 1, 0];
+    if(num < 0)
+    {
+        return "OOPS";
+    }
+    {
+        for(let i = 0; i < num; i++)
+        {
+            arr[2] = arr[0] + arr[1];
+            arr[1] = arr[0];
+            arr[0] = arr[2];
+        }
+        return arr[2];
+    }
 };
 
 // Do not edit below this line
